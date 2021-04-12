ca17fc1b359d5798944af6cfa53584bf967c9d75
me <learn.ayham@zoho.com>
2021-04-12 21:25:40 +0200

    FINAL: a working model

--- model.py
+++ model.py
@@ -25,16 +25,16 @@ class Actor(nn.Module):
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        
+
         #minimize variations in state sizes
         #self.norm_1 = nn.BatchNorm1d(state_size)
-        
+
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         #self.dropout = nn.Dropout(0.5) # not useful, the model is barely learning!
         self.fc3 = nn.Linear(fc2_units, 128)
         self.fc4 = nn.Linear(128, action_size)
-      
+
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -42,19 +42,19 @@ class Actor(nn.Module):
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
         self.fc4.weight.data.uniform_(-3e-3, 3e-3)
-        
+
         #self.fc6.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        
 
-        
+
+
         '''
         # input in pytoch is 2D or 3D.
         #https://stackoverflow.com/questions/53500511/valueerror-expected-2d-or-3d-input-got-1d-input-pytorch
         #x = x[None, ...]
-        
+
         if state.dim()==1:
             state = torch.unsqueeze(state,0)
         '''
@@ -65,7 +65,7 @@ class Actor(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.tanh(self.fc4(x)) #restrict output to (-1,1)
-        
+
         return x
 
 
@@ -84,25 +84,24 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        
+
         #self.norm_1 = nn.BatchNorm1d(state_size)
-        
+
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        
         self.fc3 = nn.Linear(fc2_units, 128)
         self.fc4 = nn.Linear(128, 1)
-        
+
         #self.fc6 = nn.Linear(128, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
-        
+
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
         self.fc4.weight.data.uniform_(-3e-3, 3e-3)
-        
+
         #self.fc6.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
@@ -110,17 +109,17 @@ class Critic(nn.Module):
         '''
         if state.dim()==1:
             state = torch.unsqueeze(state,0)
-        
+
         xs = self.norm_1(state)
         '''
-        
-        
+
+
         xs = F.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         #x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
-  
-        
+
+
         return x
